48dd6d977a830d6a08cfb7eb9e846a4cdd1e9bdc
Ryan Petrich <rpetrich@gmail.com>
2016-09-05 18:52:12 -0400

    Refactor parser into its own module

--- src/swift-to-js.js
+++ src/swift-to-js.js
@@ -14,341 +14,9 @@ var enums = stdlib.enums;
 var builtins = stdlib.builtins;
 var usedBuiltins = {};
 
-// Current parse state
-var declarations = {};
-var currentDeclaration;
-var currentBasicBlock;
+var Parser = require("./parser.js");
 
-// Lookback, to steal some unmangled name information that swiftc sticks in a comment
-var lookbackLine;
-
-function splitNoParens(s) {
-	var parens = /\(|\)|\<|\-?\>/g;
-	var result = s.split(",");
-	for (var i = 0; i < result.length; i++) {
-		do {
-			var opens = 0;
-			var find = null;
-			while ((find = parens.exec(result[i])) !== null) {
-				switch (find[0]) {
-					case "(":
-					case "<":
-						opens++;
-						break;
-					case ")":
-					case ">":
-						opens--;
-						break;
-				}
-			}
-			if (i + 1 >= result.length) {
-				break;
-			}
-			if (opens > 0) {
-				result[i] += result.splice(i + 1, 1);
-			}
-		} while(opens);
-		result[i] = result[i].trim();
-    }
-    return result;
-}
-
-function parseSil(line) {
-	var name = line.split(/:/)[0].split(/\s+/).filter(part => /^@/.test(part))[0].substring(1);
-	var declaration = {
-		name: name,
-		basicBlocks: [],
-	};
-	if (!/\b(hidden|shared_external)\b/.test(line)) {
-		var beautifulMatch = lookbackLine.match(/^\/\/ \w+\.(\w+)/);
-		if (beautifulMatch) {
-			declaration.beautifulName = beautifulMatch[1];
-		}
-	}
-	if (/{$/.test(line)) {
-		if (currentDeclaration) {
-			throw "Already inside a declaration!";
-		}
-		currentDeclaration = declaration;
-		currentBasicBlock = undefined;
-	}
-	declarations[name] = declaration;
-}
-
-function parseBasicBlock(line) {
-	if (!currentDeclaration) {
-		throw "Found a basic block declaration outside of function declaration!";
-	}
-	var argMatch = line.match(/\((.*)\)/);
-	if (argMatch) {
-		var args = splitNoParens(argMatch[1]).map(arg => arg.match(/^%(\d+)/)[1])
-	}
-	currentBasicBlock = {
-		name: line.match(/^\w+\b/)[0],
-		arguments: args || [],
-		instructions: [],
-		references: [],
-		backReferences: [],
-	}
-	currentDeclaration.basicBlocks.push(currentBasicBlock);
-}
-
-function parseInstruction(line) {
-	if (/^debug_value\s/.test(line)) {
-		return;
-	}
-	if (/^debug_value_addr\s/.test(line)) {
-		return;
-	}
-	if (/^retain_value\s+/.test(line)) {
-		return;
-	}
-	if (/^release_value\s+/.test(line)) {
-		return;
-	}
-	if (/^dealloc_stack\s+/.test(line)) {
-		return;
-	}
-	if (/^strong_retain\s+/.test(line)) {
-		return;
-	}
-	if (/^strong_release\s+/.test(line)) {
-		return;
-	}
-	if (line == "unreachable") {
-		return {
-			type: "unreachable"
-		};
-	}
-	var match = line.match(/^\%(\w+)\s*=\s*(\w+)\s*(.*)/);
-	if (match) {
-		var assignment = {
-			type: "assignment",
-			destinationLocalName: match[1],
-			instruction: match[2],
-		};
-		var args = match[3];
-		switch (assignment.instruction) {
-			case "integer_literal":
-				assignment.value = args.split(",")[1].trim();
-				break;
-			case "string_literal":
-				assignment.value = args.match(/\".*\"/)[0];
-				break;
-			case "enum":
-				var enumMatch = args.match(/^\$(.*),\s+.*?\.(\w+)\!.*(,\s+\%\w+\s+:)?/);
-				assignment.enumName = enumMatch[1];
-				assignment.caseName = enumMatch[2];
-				if (enumMatch[3]) {
-					assignment.sourceLocalName = enumMatch[3].match(/^,\s+\%(\w+)\s+:$/)[1];
-				}
-				break;
-			case "struct":
-				var structMatch = args.match(/^\$(.*?)\s+\((.*)\)/);
-				assignment.structName = basicNameForStruct(structMatch[1]);
-				assignment.arguments = splitNoParens(structMatch[2]).map(arg => arg.match(/^%(\d+)/)[1]);
-				break;
-			case "tuple":
-				var match = args.match(/^\((.*)\)/);
-				if (match && match[1]) {
-					assignment.arguments = splitNoParens(match[1]).map(arg => arg.match(/^%(\d+)/)[1]);
-				} else {
-					assignment.arguments = [];
-				}
-				break;
-			case "struct_extract":
-				var structMatch = args.match(/^%(\d+)\s*:\s*.*\.(.*)$/);
-				assignment.sourceLocalName = structMatch[1];
-				assignment.fieldName = structMatch[2];
-				break;
-			case "tuple_extract":
-				var tupleMatch = args.match(/^%(\d+)\s*:.*,\s*(\d+)$/);
-				assignment.sourceLocalName = tupleMatch[1];
-				assignment.fieldIndex = tupleMatch[2];
-				break;
-			case "builtin":
-				var builtinMatch = args.match(/^\"(\w+)\"(<\w+>)?\((.*)\)\s*:/);
-				assignment.builtinName = builtinMatch[1];
-				assignment.arguments = splitNoParens(builtinMatch[3]).map(arg => arg.match(/^%(\d+)/)[1]);
-				break;
-			case "function_ref":
-				var functionMatch = args.match(/^@(\w+)\s*:/);
-				assignment.functionName = functionMatch[1];
-				break;
-			case "apply":
-				var applyMatch = args.match(/^(\[nothrow\]\s+)?%(\d+)\((.*)\)\s*:/);
-				assignment.sourceLocalName = applyMatch[2];
-				assignment.arguments = splitNoParens(applyMatch[3]).map(arg => arg.match(/^%(\d+)$/)[1]);
-				break;
-			case "alloc_stack":
-				break;
-			case "alloc_box":
-				break;
-			case "project_box":
-				var boxMatch = args.match(/^%(\w+)\s+:/);
-				assignment.sourceLocalName = boxMatch[1];
-				break;
-			case "struct_element_addr":
-				var structMatch = args.match(/^%(\w+)\s+:\s+.*?#(\w+)\.(\w+)$/);
-				assignment.sourceLocalName = structMatch[1];
-				assignment.structName = structMatch[2];
-				assignment.fieldName = structMatch[3];
-				break;
-			case "load":
-				var loadMatch = args.match(/^%(\w+)\s+:/);
-				assignment.sourceLocalName = loadMatch[1];
-				break;
-			case "unchecked_enum_data":
-				var enumMatch = args.match(/^%(\w+)\s+:/);
-				assignment.sourceLocalName = enumMatch[1];
-				break;
-			case "unchecked_addr_cast":
-			case "pointer_to_address":
-			case "ref_to_raw_pointer":
-			case "raw_pointer_to_ref":
-				var match = args.match(/^%(\w+)\s+:/);
-				assignment.sourceLocalName = match[1];
-				break;
-			case "index_raw_pointer":
-				var match = args.match(/^%(\w+)\s+:.*?,\s+%(\w+)\s+:/)
-				assignment.sourceLocalName = match[1];
-				assignment.offsetLocalName = match[2];
-				break;
-			default:
-				assignment.unparsedArguments = args;
-				break;
-		}
-		return assignment;
-	}
-	match = line.match(/^return\s+\%(\w+) :/);
-	if (match) {
-		return {
-			type: "return",
-			localName: match[1],
-		};
-	}
-	match = line.match(/^br\s+(\w+)\((.*)\)/) || line.match(/^br\s+(\w+)/);
-	if (match) {
-		return {
-			type: "branch",
-			blockName: match[1],
-			arguments: match[2] ? splitNoParens(match[2]).map(arg => arg.match(/^%(\d+)/)[1]) : [],
-		};
-	}
-	match = line.match(/^cond_br\s+\%(\w+),\s*(\w+),\s(\w+)/);
-	if (match) {
-		return {
-			type: "conditional_branch",
-			localName: match[1],
-			trueBlockName: match[2],
-			falseBlockName: match[3],
-		};
-	}
-	match = line.match(/^store\s+\%(\w+)\s+to\s+\%(\w+)\s+:/);
-	if (match) {
-		return {
-			type: "store",
-			sourceLocalName: match[1],
-			destinationLocalName: match[2],
-		};
-	}
-	match = line.match(/^switch_enum\s+\%(\w+)\s+:\s+.*?,\s+(case .*)/);
-	if (match) {
-		return {
-			type: "switch_enum",
-			localName: match[1],
-			cases: splitNoParens(match[2]).map(arg => {
-				var match = arg.match(/^case\s+\#(.*):\s+(.*)$/);
-				return {
-					"case": match[1],
-					"basicBlock": match[2]
-				};
-			})
-		};
-	}
-	match = line.match(/^try_apply\s+%(\w+)\((.*)\)\s+:.*,\s+normal\s+(\w+),\s+error\s+(\w+)/);
-	if (match) {
-		return {
-			type: "try_apply",
-			localName: match[1],
-			arguments: splitNoParens(match[2]).map(arg => arg.match(/^%(\d+)$/)[1]),
-			normalBlockName: match[3],
-			errorBlockName: match[4],
-		};
-	}
-	match = line.match(/^throw\s+%(\w+)\s*:/);
-	if (match) {
-		return {
-			type: "throw",
-			name: match[1],
-		};
-	}
-	throw "Unknown instruction: " + line;
-}
-
-function parse(originalLine) {
-	line = originalLine.replace(/\s*\/\/.*/, "");
-	if (line.length != 0) {
-		var directive = line.match(/^\w+\b/);
-		if (directive) {
-			directive = directive[0];
-			switch (directive) {
-				case "sil_stage":
-					// Do nothing with sil_stage directives
-					break;
-				case "import":
-					// Do nothing with import directives
-					break;
-				case "sil":
-					parseSil(line);
-					break;
-				default:
-					if (/^\w+(\(.*\))?:$/.test(line)) {
-						// Found basic block!
-						parseBasicBlock(line);
-					}
-					break;
-			}
-		} else if (/}$/.test(line)) {
-			if (currentDeclaration) {
-				currentDeclaration = undefined;
-				currentBasicBlock = undefined;
-			} else {
-				// Not inside a declaration!
-				// Should be an error, but we aren't even close to understanding Swift's protocols/method tables
-			}
-		} else if (/^  /.test(line)) {
-			if (currentBasicBlock) {
-				var instruction = parseInstruction(line.trim());
-				if (instruction) {
-					currentBasicBlock.instructions.push(instruction);
-				}
-			} else {
-				// Not inside a declaration or basic block!
-				// Should be an error, but we aren't even close to understanding Swift's protocols/method tables
-			}
-		} else {
-			console.log("Unknown: " + line);
-		}
-	}
-	lookbackLine = originalLine;
-}
-
-function findBasicBlock(blocks, name) {
-	for (var i = 0; i < blocks.length; i++) {
-		if (blocks[i].name == name) {
-			return blocks[i];
-		}
-	}
-}
-
-function basicNameForStruct(structName) {
-	return structName.match(/^\w+/)[0];
-}
-
-function caseNameForEnum(fullEnumName) {
-	return fullEnumName.match(/^\w+\.(\w+)\!/)[1];
-}
+var parser = new Parser();
 
 function IndentedBuffer(){
     this.lines = [];
@@ -368,6 +36,18 @@ IndentedBuffer.prototype.write = function (line, extra) {
 		this.indent(-extra);
 	}
 };
+
+function findBasicBlock(blocks, name) {
+	for (var i = 0; i < blocks.length; i++) {
+		if (blocks[i].name == name) {
+			return blocks[i];
+		}
+	}
+}
+
+function caseNameForEnum(fullEnumName) {
+	return fullEnumName.match(/^\w+\.(\w+)\!/)[1];
+}
 
 function writeBranchToBlock(targetBlock, buffer, siblingBlocks) {
 	var index = siblingBlocks.indexOf(targetBlock);
@@ -399,7 +79,7 @@ function writeBasicBlock(basicBlock, buffer, siblingBlocks) {
 						declaration += instruction.value;
 						break;
 					case "enum":
-						var enumName = basicNameForStruct(instruction.enumName);
+						var enumName = instruction.enumName;
 						var enumLayout = enums[enumName];
 						if (!enumLayout) {
 							throw "Unable to find enum: " + enumName;
@@ -589,8 +269,8 @@ function analyzeBlockReferences(basicBlocks) {
 }
 
 function writeAST(buffer) {
-	for (var name in declarations) {
-		var basicBlocks = declarations[name].basicBlocks;
+	for (var name in parser.declarations) {
+		var basicBlocks = parser.declarations[name].basicBlocks;
 		if (basicBlocks.length == 0) {
 			// No basic blocks, some kind of weird declaration we don't support yet
 			continue;
@@ -621,7 +301,7 @@ function writeAST(buffer) {
 		}
 		buffer.indent(-1);
 		buffer.write("}");
-		var beautifulName = declarations[name].beautifulName;
+		var beautifulName = parser.declarations[name].beautifulName;
 		if (beautifulName) {
 			buffer.write("window[\"" + beautifulName + "\"] = " + name + ";");
 		}
@@ -637,7 +317,7 @@ var rl = readline.createInterface({
 
 rl.on("line", function(line){
 	//try {
-		parse(line);
+		parser.addLine(line);
 	//} catch (e) {
 	//	throw e.toString() + " on line " + line;
 	//}
