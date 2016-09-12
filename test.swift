faf7b835d1a87fb70dd5d4fe30538dec15007f47
Ryan Petrich <rpetrich@gmail.com>
2016-09-12 02:25:18 -0400

    Add updated test

--- test.swift
+++ test.swift
@@ -1,34 +1,36 @@
-func increment(a: Int) -> Int {
-    return a + 1
+import Darwin
+
+func increment(number: Int) -> Int {
+    return number + 1
 }
 
-public func increment_until_zero(a: Int) -> Int {
-    if (a < 0) {
-        return increment(a)
+public func increment_until_zero(number: Int) -> Int {
+    if (number < 0) {
+        return increment(number: number)
     }
-    return a
+    return number
 }
 
-public func decrement_until_zero(a: Int) -> Int {
-    if (a > 0) {
-        return a - 1
+public func decrement_until_zero(number: Int) -> Int {
+    if (number > 0) {
+        return number - 1
     }
-    return a
+    return number
 }
 
-public func negate(a: Int) -> Int {
-    return -a
+public func negate(number: Int) -> Int {
+    return -number
 }
 
 public func factorial_recursive(number: Int) -> Int {
     if (number <= 1) {
         return 1
     }
-    return number * factorial_recursive(number - 1)
+    return number * factorial_recursive(number: number - 1)
 }
 
 public func factorial_iterative(number: Int) -> Int {
-    return number < 1 ? 1 : (1...number).reduce(1, combine: *)
+    return number < 1 ? 1 : (1...number).reduce(1, *)
 }
 
 public func silly_math(num: Int) -> Int {
@@ -39,6 +41,39 @@ public func silly_math(num: Int) -> Int {
     return result;
 }
 
+public func more_silly_math(num: Int) -> Int {
+    var result: Int
+    if (num < 0) {
+        result = num;
+    } else {
+        result = -num;
+    }
+    return result * 1000 + 4;
+}
+
+public func optional_from(num: Int) -> Bool? {
+    if (num > 0) {
+        return true
+    } else if (num == 0) {
+        return false
+    }
+    return .none
+}
+
+public func description_of(option: Bool?) -> String {
+    if let unwrapped = option {
+        if unwrapped {
+            return "Greater than zero"
+        }
+        return "Equal to zero"
+    }
+    return "Less than zero"
+}
+
+public func has_value(option: Bool?) -> Bool {
+    return option != nil
+}
+
 public func hello_world() -> String {
     return "Hello World!"
 }
@@ -47,6 +82,72 @@ public func string_length(str: String) -> Int {
     return str.utf16.count;
 }
 
-/*public func concat(a: String, b: String) -> String {
-    return a + b
-}*/
+// public enum Divisible {
+//     case byNone
+//     case byThree
+//     case byFive
+//     case byBoth
+// }
+
+// public func select_value(num: Int) -> Divisible {
+//     switch num % 15 {
+//         case 0:
+//             return Divisible.byBoth
+//         case 3, 6, 9, 12:
+//             return Divisible.byThree
+//         case 5, 10:
+//             return Divisible.byFive
+//         default:
+//             return Divisible.byNone
+//     }
+// }
+
+public struct Point {
+    var x: Double
+    var y: Double
+    public init() {
+        x = 0
+        y = 0
+    }
+    public init(x _x: Double, y _y: Double) {
+        x = _x
+        y = _y
+    }
+    var isZero: Bool {
+        get {
+            return x == 0 && y == 0
+        }
+    }
+}
+
+public func distance(first: Point, second: Point) -> Double {
+    let delta = Point(x: first.x - second.x, y: first.y - second.y)
+    return sqrt(delta.x * delta.x + delta.y * delta.y)
+}
+
+public class IntHolder {
+    var value: Int = 0
+}
+
+public func getValue(holder: IntHolder) -> Int {
+    return holder.value;
+}
+
+public func newValue(val: Int) -> IntHolder {
+    let result = IntHolder()
+    result.value = val;
+    return result;
+}
+
+public func updateValue(holder: IntHolder, value: Int) {
+    holder.value = value;
+}
+
+public final class IntHolderSubclass : IntHolder {
+    var wasSet: Bool = false
+    override var value: Int {
+        didSet {
+            wasSet = true
+        }
+    }
+}
