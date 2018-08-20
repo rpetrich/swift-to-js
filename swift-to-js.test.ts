1977f8d1c9aafeb966045458928b433537806d16
Ryan Petrich <rpetrich@gmail.com>
2018-08-19 20:07:01 -0400

    Have tests continue to work after macOS drops its silly .DS_Store files

--- swift-to-js.test.ts
+++ swift-to-js.test.ts
@@ -1,6 +1,6 @@
 import { compile } from "./swift-to-js";
 
-import { readdirSync, readFile as readFile_, writeFile as writeFile_ } from "fs";
+import { readdirSync, statSync, readFile as readFile_, writeFile as writeFile_ } from "fs";
 import { promisify } from "util";
 
 const writeOutput = false;
@@ -11,21 +11,23 @@ const writeFile = promisify(writeFile_);
 const swiftFilePattern = /\.swift$/;
 
 for (const category of readdirSync("./tests/")) {
-	describe(category, () => {
-		for (const file of readdirSync(`./tests/${category}/`)) {
-			if (swiftFilePattern.test(file)) {
-				test(file, async () => {
-					const swiftPath = `./tests/${category}/${file}`;
-					const jsPath = `./tests/${category}/${file.replace(swiftFilePattern, ".js")}`;
-					const result = compile(swiftPath);
-					if (writeOutput) {
-						await writeFile(jsPath, await result);
-					} else {
-						const expected = readFile(jsPath);
-						expect(await result).toEqual((await expected).toString("utf-8"));
-					}
-				});
+	if (statSync(`./tests/${category}`).isDirectory()) {
+		describe(category, () => {
+			for (const file of readdirSync(`./tests/${category}`)) {
+				if (swiftFilePattern.test(file)) {
+					test(file.replace(swiftFilePattern, ""), async () => {
+						const swiftPath = `./tests/${category}/${file}`;
+						const jsPath = `./tests/${category}/${file.replace(swiftFilePattern, ".js")}`;
+						const result = compile(swiftPath);
+						if (writeOutput) {
+							await writeFile(jsPath, await result);
+						} else {
+							const expected = readFile(jsPath);
+							expect(await result).toEqual((await expected).toString("utf-8"));
+						}
+					});
+				}
 			}
-		}
-	});
+		});
+	}
 }
