0d51570de4fa00142795c865f3a0e6924927c061
Ryan Petrich <rpetrich@gmail.com>
2018-09-03 14:29:56 -0400

    Add test for dictionary of optional value

--- tests/dictionaries/int-get.js
+++ tests/dictionaries/int-get.js
@@ -1,3 +1,6 @@
 export function dictGet$key$(dict, key) {
   return Object.hasOwnProperty.call(dict, key) ? dict[key] : null;
 }
+export function doubleDictGet$key$(dict, key) {
+  return Object.hasOwnProperty.call(dict, key) ? [dict[key]] : [];
+}
--- tests/dictionaries/int-get.swift
+++ tests/dictionaries/int-get.swift
@@ -1,3 +1,7 @@
 public func dictGet(_ dict: [Int: Int], key: Int) -> Int? {
     return dict[key]
 }
+
+public func doubleDictGet(_ dict: [Int: Int?], key: Int) -> Int?? {
+    return dict[key]
+}
