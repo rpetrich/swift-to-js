6ae1b082a89483073d30cbce0dc8ac0a47fd828e
Ryan Petrich <rpetrich@gmail.com>
2018-09-30 17:51:50 -0400

    Unlink test fixtures on error when in write output mode so it's clearer that tests aren't passing

--- swift-to-js.test.ts
+++ swift-to-js.test.ts
@@ -1,12 +1,13 @@
 import { compile } from "./swift-to-js";
 
-import { readdirSync, readFile as readFile_, statSync, writeFile as writeFile_ } from "fs";
+import { readdirSync, readFile as readFile_, statSync, unlink as unlink_, writeFile as writeFile_ } from "fs";
 import { promisify } from "util";
 
 const writeOutput = false;
 
 const readFile = promisify(readFile_);
 const writeFile = promisify(writeFile_);
+const unlink = promisify(unlink_);
 
 const swiftFilePattern = /\.swift$/;
 
@@ -20,8 +21,18 @@ for (const category of readdirSync("./tests/")) {
 						const jsPath = `./tests/${category}/${file.replace(swiftFilePattern, ".js")}`;
 						const result = compile(swiftPath);
 						if (writeOutput) {
-							const { code, ast } = await result;
-							await Promise.all([writeFile(jsPath, code), writeFile(swiftPath + ".ast", ast)]);
+							try {
+								const { code, ast } = await result;
+								await Promise.all([writeFile(jsPath, code), writeFile(swiftPath + ".ast", ast)]);
+							} catch (e) {
+								try {
+									statSync(jsPath);
+									await unlink(jsPath);
+								} catch (e) {
+									// Ignore
+								}
+								throw e;
+							}
 						} else {
 							const expected = readFile(jsPath);
 							expect((await result).code).toEqual((await expected).toString("utf-8"));
