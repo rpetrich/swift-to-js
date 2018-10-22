6b134c35ca83648058d92ae0b5142b389b8fde55
Ryan Petrich <rpetrich@gmail.com>
2018-10-21 23:06:08 -0400

    Cache parse tree of common types

--- parse.ts
+++ parse.ts
@@ -3,7 +3,7 @@ import { parse as declaration } from "./declaration";
 import { parse as type, Function } from "./types";
 
 export const parseDeclaration = parse(declaration, "declaration");
-export const parseType = parse(type, "type");
+export const parseType = parse(type, "type", "Bool", "Int", "String", "Hashable", "Hasher", "Equatable", "Comparable", "BinaryInteger", "Numeric", "SignedInteger", "SignedNumeric", "FixedWidthInteger", "Strideable", "CustomStringConvertible", "LosslessStringConvertible", "Collection", "BidirectionalCollection", "Type", "T", "T.Type", "() -> Void", "(Self, Self) -> Self", "(inout Self, Self) -> Void", "(Self, Self) -> Bool", "(Self, inout Hasher) -> Bool", "(Self) -> Int");
 export const parseAST = parse(ast, "ast");
 
 export function parseFunctionType(text: string): Function {
@@ -14,16 +14,27 @@ export function parseFunctionType(text: string): Function {
 	return result;
 }
 
-function parse<T>(parser: (text: string) => T, description: string): (text: string) => T {
-	// const memoized: { [name: string]: T } = Object.create(null);
-	return (text: string) => {
+function parse<T>(parser: (text: string) => T, description: string, ...precomputedKeys: string[]): (text: string) => T {
+	if (precomputedKeys.length === 0) {
+		return (text: string): T => {
+			try {
+				return parser(text);
+			} catch (e) {
+				console.error(`${description}: ${text}`);
+				throw e;
+			}
+		};
+	}
+	const precomputed: { [name: string]: T } = Object.create(null);
+	for (const key of precomputedKeys) {
+		precomputed[key] = parser(key);
+	}
+	return (text: string): T => {
+		if (Object.hasOwnProperty.call(precomputed, text)) {
+			return precomputed[text];
+		}
 		try {
 			return parser(text);
-			// if (Object.hasOwnProperty.call(memoized, text)) {
-			// 	return memoized[text];
-			// } else {
-			// 	return memoized[text] = parser(text);
-			// }
 		} catch (e) {
 			console.error(`${description}: ${text}`);
 			throw e;
