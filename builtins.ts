5a66fb37013eb71edb13b38f59d783d349065ce4
Ryan Petrich <rpetrich@gmail.com>
2018-11-10 12:01:10 -0500

    Fix match operator not calling through to ~= method on Equatable conformance

--- builtins.ts
+++ builtins.ts
@@ -2740,7 +2740,11 @@ export const functions: FunctionMap = {
 			);
 		});
 	},
-	"~=": (scope, arg) => binary("===", arg(1, "pattern"), arg(2, "value"), scope),
+	"~=": (scope, arg) => {
+		const T = arg(0, "T");
+		const result = call(functionValue("~=", conformance(T, "Equatable", scope), "(T.Type, T, T) -> () -> Bool"), [T, arg(1, "pattern"), arg(2, "value")], [typeValue("Type"), T, T], scope);
+		return call(result, [], [], scope);
+	},
 	"print(_:separator:terminator:)": (scope, arg, type) => call(member("console", "log", scope), [arg(0, "items")], [dummyType], scope),
 	"precondition(_:_:file:line:)": (scope, arg, type) => statements([
 		ifStatement(
